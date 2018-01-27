34c8692cf18d9106d883f57c17f76f0b66dfd3c7
Sarju <charming.sap@gmail.com>
2018-01-27 11:43:15 -0900

    Assignment 10

---
+++ Assignment10/Exercise2.ts
@@ -0,0 +1,16 @@
+class Shape {
+    constructor(public width: number = 0, public length: number = 0) { }
+}
+
+class Rectangle extends Shape {
+    constructor(public width: number, public length: number) {
+        super(width, length)
+    }
+
+    calcSize(): number {
+        return this.width * this.length;
+    }
+}
+
+var rect = new Rectangle(5, 2);
+console.log(rect.calcSize());
